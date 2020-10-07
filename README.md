6fa53d682f68da000e4e114372fd35face8b773e
isizwe <isizwe.madalane@gmail.com>
2020-10-07 07:44:30 +0200

    Add frontend

---
+++ README.md
@@ -0,0 +1,2 @@
+# Realtime Chat App
+This web app was built using Go as the backend and ReactJS as the frontend, it is a simple chat application that can handle mutliple users
---
+++ frontend/src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import './App.css';
+import { connect, sendMsg } from "./api";
+import Header from './components/Header';
+import ChatHistory from './components/ChatHistory';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chatHistory: []
+    }
+  }
+
+  componentDidMount() {
+    connect((msg) => {
+      console.log("New Message")
+      this.setState(prevState => ({
+        chatHistory: [...this.state.chatHistory, msg]
+      }))
+      console.log(this.state);
+    });
+  }
+
+  send() {
+    console.log("Hello");
+    sendMsg("Hello");
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <ChatHistory chatHistory={this.state.chatHistory} />
+        <button onClick={this.send}>Button press</button>
+      </div>
+    );
+  }
+}
+
+export default App;
